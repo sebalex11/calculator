0ee777890bde817591614fc43f54cbb73179c799
Sebastian Matthews <admiraloven@gmail.com>
2017-02-21 17:19:55 -0500

    finished up basic styling and added buttons, now I am starting to work on the javascript side of things

--- Script/Main.js
+++ Script/Main.js
@@ -1,9 +1,26 @@
 $(function(){
 
-	$("h1").on("click", function(){
-		alert("dont do that")
+	$(".number").on("click", function(){
+		
+
+		var buttonPressed = $(this).attr("data-num");
+		var buttonNum= parseInt(buttonPressed);
+		var digi = $("#digi").html();
+
+
+
+		if( $("#digi").html().indexOf("0") == 0){
+
+			$("#digi").html(digi.slice(1) + buttonNum);	
+		} else {
+			$("#digi").html(digi + buttonNum);
+		}
+
+		if(digi.length >12){
+			$("#digi").html("Limit Reached");
+			setTimeout(function(){
+				$("#digi").html("0")
+			}, 1500)
+		}
 	})
-
-	$("#display").html("Display")
-
 })
