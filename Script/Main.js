c764b47961d21b720d0ed1648d133f6dad63a944
Sebastian Matthews <admiraloven@gmail.com>
2017-02-23 13:35:09 -0500

    I did a lot. 1.Added all of the buttons, 2. Finalized the style, 3. Added most of the javascript

--- Script/Main.js
+++ Script/Main.js
@@ -1,20 +1,28 @@
 $(function(){
 
+	var operands = ["+","-", "/", "x"]
+
+//if you press a button this function handles the event
+
 	$(".number").on("click", function(){
 		
-
+//set variables to make calling easier
 		var buttonPressed = $(this).attr("data-num");
 		var buttonNum= parseInt(buttonPressed);
 		var digi = $("#digi").html();
 
 
-
-		if( $("#digi").html().indexOf("0") == 0){
+//if the variables are 0 or a operator take it away and do numbers, else just put the numbers
+		if( ($("#digi").html().indexOf("0") == 0) || (operands.indexOf($("#digi").html()) >= 0)){
 
 			$("#digi").html(digi.slice(1) + buttonNum);	
 		} else {
 			$("#digi").html(digi + buttonNum);
 		}
+
+//if the number gets too long, kill it  
+
+//>>>>>I HAVE TO PUT THIS FOR THE EQUALS FUNCTION TOO<<<<<
 
 		if(digi.length >12){
 			$("#digi").html("Limit Reached");
@@ -23,4 +31,30 @@ $(function(){
 			}, 1500)
 		}
 	})
+
+
+//This handles the operator button event
+	$(".operand").on("click", function(){
+		var buttonPressed = $(this).attr("data-num");
+		var $running = $("#running");
+		var $digi = $("#digi");
+
+//testing area
+		console.log("this is what was in running last " +$running.html())
+		console.log("this is the lenght of the last running " +$running.html().length)
+		console.log("this is what the  last digit of running was " +$running.html()[$running.html().length -1])
+
+
+//if digi doesnt include an operator add the numbers from digi and append the operator. 
+		if(operands.indexOf($digi.html()) < 0){
+			$running.html($running.html()+$digi.html()+buttonPressed);
+			$digi.html(buttonPressed);
+		}
+	})
+
+
+
+
+
+
 })
