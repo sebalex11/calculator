6720b7958ed2350c021084d0b1ceb1bb46171d96
Sebastian Matthews <admiraloven@gmail.com>
2017-02-23 16:43:29 -0500

    finished up the javascript for calculations only need clear buttons

--- Script/Main.js
+++ Script/Main.js
@@ -1,6 +1,8 @@
 $(function(){
 
 	var operands = ["+","-", "/", "x"]
+	var $running = $("#running");
+	var $digi = $("#digi");
 
 //if you press a button this function handles the event
 
@@ -36,23 +38,75 @@ $(function(){
 //This handles the operator button event
 	$(".operand").on("click", function(){
 		var buttonPressed = $(this).attr("data-num");
-		var $running = $("#running");
-		var $digi = $("#digi");
 
-//testing area
+
+//Testing area
+
+/*
 		console.log("this is what was in running last " +$running.html())
 		console.log("this is the lenght of the last running " +$running.html().length)
 		console.log("this is what the  last digit of running was " +$running.html()[$running.html().length -1])
-
+*/
 
 //if digi doesnt include an operator add the numbers from digi and append the operator. 
 		if(operands.indexOf($digi.html()) < 0){
-			$running.html($running.html()+$digi.html()+buttonPressed);
+			$running.html($running.html()+$digi.html()+" "+buttonPressed+" ");
 			$digi.html(buttonPressed);
 		}
 	})
 
+//This is going to do the equals
 
+$(".equals").on("click", function(){
+
+//make sure the last digit was a number
+	if(operands.indexOf($digi.html()) < 0){
+	$running.html($running.html()+$digi.html());
+
+//turn all entries into an array
+	var total = $running.html().split(" ");
+//start the calculations
+	total = total.reduce(function(acc, val, ind){
+
+//the first iteration sets the amount
+		if(ind == 0){
+			return parseInt(val);
+		} else if(operands.indexOf(val)<0){
+			var tempNum = parseInt(val);
+			switch(total[ind-1]){
+				case "+":
+					return acc + tempNum;
+					break;
+				case "-":
+					return acc - tempNum;
+					break;
+				case "/":
+					return acc / tempNum;
+					break;
+				case "x": 
+					return acc * tempNum;
+					break;
+			}
+		} else {
+			return acc;
+		}
+	},0)
+	if(String(total).length > 12){
+		
+		$("#digi").html("Limit Reached");
+		
+		setTimeout(function(){
+				$("#digi").html("0")
+		}, 1500)
+		$running.html("")
+	} else {
+		$digi.html(total);
+		$running.html("")
+	}
+
+
+	}
+})
 
 
 
