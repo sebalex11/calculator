0af33431b2647d448b9d1165aa506fac216adc3e
Sebastian Matthews <admiraloven@gmail.com>
2017-02-24 16:20:45 -0500

    Calculator JS should be finished. Must restyle later

--- Script/Main.js
+++ Script/Main.js
@@ -3,17 +3,53 @@ $(function(){
 	var operands = ["+","-", "/", "x"]
 	var $running = $("#running");
 	var $digi = $("#digi");
+	var exe;
+
+
+	var clearAll = function(){
+		if($running.html().length >0 || $digi.html().length > 0){
+			$running.html("");
+			$digi.html("");
+		}
+	}
+
+	var checkDecimal = 	function(){
+
+							if($digi.html().indexOf(".") < 0 && operands.indexOf($digi.html()) < 0){
+								if($digi.html().length >12){
+									$("#digi").html("Limit Reached");
+									setTimeout(function(){
+										$("#digi").html("0")
+									}, 1500)
+								} else {
+									$digi.html($digi.html()+".")
+									}
+							}
+						}
+
+
+	var clear = 	function(){
+						if($digi.html().length >0 && operands.indexOf($digi.html()) < 0){
+							$digi.html("");
+						}
+					}
 
 //if you press a button this function handles the event
 
 	$(".number").on("click", function(){
-		
+
+		console.log(exe)
+		console.log($digi.html())
+
+		if(exe == "finished"){
+			exe = "";
+			clearAll();
+		}
+
 //set variables to make calling easier
 		var buttonPressed = $(this).attr("data-num");
-		var buttonNum= parseInt(buttonPressed);
+		var buttonNum = buttonPressed;
 		var digi = $("#digi").html();
-
-
 //if the variables are 0 or a operator take it away and do numbers, else just put the numbers
 		if( ($("#digi").html().indexOf("0") == 0) || (operands.indexOf($("#digi").html()) >= 0)){
 
@@ -38,24 +74,15 @@ $(function(){
 //This handles the operator button event
 	$(".operand").on("click", function(){
 		var buttonPressed = $(this).attr("data-num");
-
-
-//Testing area
-
-/*
-		console.log("this is what was in running last " +$running.html())
-		console.log("this is the lenght of the last running " +$running.html().length)
-		console.log("this is what the  last digit of running was " +$running.html()[$running.html().length -1])
-*/
-
-//if digi doesnt include an operator add the numbers from digi and append the operator. 
+		
+//If digi doesnt include an operator add the numbers from digi and append the operator. 
 		if(operands.indexOf($digi.html()) < 0){
 			$running.html($running.html()+$digi.html()+" "+buttonPressed+" ");
 			$digi.html(buttonPressed);
 		}
 	})
 
-//This is going to do the equals
+//This is going to handle the "equals" button
 
 $(".equals").on("click", function(){
 
@@ -65,14 +92,15 @@ $(".equals").on("click", function(){
 
 //turn all entries into an array
 	var total = $running.html().split(" ");
+
 //start the calculations
 	total = total.reduce(function(acc, val, ind){
-
+console.log(parseFloat(val));
 //the first iteration sets the amount
 		if(ind == 0){
-			return parseInt(val);
+			return parseFloat(val);
 		} else if(operands.indexOf(val)<0){
-			var tempNum = parseInt(val);
+			var tempNum = parseFloat(val, 10);
 			switch(total[ind-1]){
 				case "+":
 					return acc + tempNum;
@@ -103,12 +131,18 @@ $(".equals").on("click", function(){
 		$digi.html(total);
 		$running.html("")
 	}
-
+	exe = "finished"
 
 	}
-})
+ })
+
+//EVENTS
+
+	$(".clear").on("click", clear)
+ 
+	$(".clearAll").on("click", clearAll)
 
 
-
+	$(".decimal").on("click", checkDecimal)
 
 })
